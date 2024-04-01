8415f73e5f461d92a28849c442c525c120433f3d
RayyanZaid <rayyanzaid0401@gmail.com>
2024-04-01 16:17:54 -0700

    custom serializers

--- store/serializers.py
+++ store/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
+from .models import Product
 
 
+from decimal import Decimal
 # Completely separate from the product class
 
 # Using this, we can go from (Product Object) --> Python Dictionary
@@ -8,4 +10,13 @@ class ProductSerializer(serializers.Serializer):
 
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+
+    # Add a custom serializer
+
+    price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+
+    def calculate_tax(self, product : Product):
+
+                      # decimal           # float
+        return round(product.unit_price * Decimal(1.1),2)
