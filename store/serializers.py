7667b182a78b13e0e58ed2e93f124ed45db16293
RayyanZaid <rayyanzaid0401@gmail.com>
2024-04-02 21:20:54 -0700

    ModelSerializer done

--- store/serializers.py
+++ store/serializers.py
@@ -5,52 +5,79 @@ from .models import Product, Collection
 from decimal import Decimal
 
 
-class CollectionSerializer(serializers.Serializer):
+# class CollectionSerializer(serializers.Serializer):
 
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
 
 
 
 # Completely separate from the product class
 
 # Using this, we can go from (Product Object) --> Python Dictionary
-class ProductSerializer(serializers.Serializer):
+# class ProductSerializer(serializers.Serializer):
 
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+#     id = serializers.IntegerField()
+#     title = serializers.CharField(max_length=255)
+#     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
 
-    # Add a custom serializer
+#     # Add a custom serializer
 
+#     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
+
+#     def calculate_tax(self, product : Product):
+
+#                       # decimal           # float
+#         return round(product.unit_price * Decimal(1.1),2)
+
+
+#     # 3 ways to include related objects
+
+#     # 1) PrimaryKeyRelatedField
+#     # include pk (id) of each collection in Product object
+#     # collection = serializers.PrimaryKeyRelatedField(
+#     #     queryset=Collection.objects.all()
+#     # )
+
+#     # 2) StringRelatedField - returns the string representation of the object
+#     # need to include .selectrelated('collection') in the views
+#     # collection = serializers.StringRelatedField()
+
+#     # 3) Use the serializer class of that object
+
+#     # collection = CollectionSerializer()
+
+#     # 4) Use HyperLink
+
+#     collection = serializers.HyperlinkedRelatedField(
+#         queryset=Collection.objects.all(),
+#         view_name = 'collection-detail'
+#     )
+
+
+# Model Serializer - don't have to redefine all the fields since they're already in the models.py
+    
+class CollectionSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Collection
+
+        fields = ['id' , 'title']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+
+        model = Product
+
+        # Django will auto create serializer
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
+    
     price_with_tax = serializers.SerializerMethodField(method_name="calculate_tax")
 
     def calculate_tax(self, product : Product):
 
-                      # decimal           # float
+                    # decimal           # float
         return round(product.unit_price * Decimal(1.1),2)
-
-
-    # 3 ways to include related objects
-
-    # 1) PrimaryKeyRelatedField
-    # include pk (id) of each collection in Product object
-    # collection = serializers.PrimaryKeyRelatedField(
-    #     queryset=Collection.objects.all()
-    # )
-
-    # 2) StringRelatedField - returns the string representation of the object
-    # need to include .selectrelated('collection') in the views
-    # collection = serializers.StringRelatedField()
-
-    # 3) Use the serializer class of that object
-
-    # collection = CollectionSerializer()
-
-    # 4) Use HyperLink
-
-    collection = serializers.HyperlinkedRelatedField(
-        queryset=Collection.objects.all(),
-        view_name = 'collection-detail'
-    )
-
